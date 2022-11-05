b51b480fec8fb9f1f55c22545d7b452dcb654689
Robingeorge12 <robingsa.btch@gmail.com>
2022-11-05 19:57:05 +0530

    add about route

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 7003
 app.get("/",(req,res)=>{
     res.send("welcome")
 })
+app.get("/about" ,(req,res)=>{
+
+    res.send("About page")
+})
+
 
 app.listen(PORT,()=>{
     console.log(`${PORT} Port`)
